26f965086f5140334d07c8cf5c9e1ac4a7f54965
Jef70987 <japhethmasinde3@gmail.com>
2025-05-19 23:50:38 +0300

    Add files via upload

--- beautyspa2/settings.py
+++ beautyspa2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o%00b$$@*fp25q^8epo_lnp-8nn75@*c!w*ppn-br8@o-)7yqt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
